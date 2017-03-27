54d9ef88a599e68911fa4ef090503decdb730730
teinted321 <darius.krei@gmail.com>
2017-03-27 20:32:32 +0300

    php

--- app.js
+++ app.js
@@ -3,33 +3,33 @@
  */
 var myApp = angular.module("myApp", ['ngRoute']);
 
-myApp.config(function ($routeProvider,$locationProvider) {
+myApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider
 
         .when('/', {
-            templateUrl : 'pages/home.html',
-            controller : 'HomeController'
+            templateUrl: 'pages/home.html',
+            controller: 'HomeController'
         })
         .when('/pirmadienis', {
-            templateUrl : 'pages/pirmadienis.html',
-            controller : 'PirmadienisController'
+            templateUrl: 'pages/pirmadienis.html',
+            controller: 'PirmadienisController'
         })
         .when('/antradienis', {
-            templateUrl : 'pages/antradienis.html',
-            controller : 'AntradienisController'
+            templateUrl: 'pages/antradienis.html',
+            controller: 'AntradienisController'
         })
-        .when('/treciadienis' , {
-            templateUrl :'pages/treciadienis.html' ,
-            controller : 'TreciadienisController'
+        .when('/treciadienis', {
+            templateUrl: 'pages/treciadienis.html',
+            controller: 'TreciadienisController'
 
         })
-        .when('/ketvirtadienis' , {
-            templateUrl : 'pages/ketvirtadienis.html' ,
-            controller : 'KetvirtadienisController'
+        .when('/ketvirtadienis', {
+            templateUrl: 'pages/ketvirtadienis.html',
+            controller: 'KetvirtadienisController'
         })
-        .when('/penktadienis' , {
-           templateUrl : 'pages/penktadienis.html' ,
-           controller : 'PenktadienisController'
+        .when('/penktadienis', {
+            templateUrl: 'pages/penktadienis.html',
+            controller: 'PenktadienisController'
         })
 
         .otherwise({redirectTo: '/'});
@@ -38,29 +38,51 @@ myApp.config(function ($routeProvider,$locationProvider) {
 
 });
 
-myApp.controller('HomeController', function ($scope){
+myApp.controller('HomeController', function ($scope) {
     $scope.message = 'Hello from HomeController';
 
 });
-myApp.controller('PirmadienisController', function ($scope){
+myApp.controller('PirmadienisController', function ($scope) {
     $scope.message = 'Hello from PirmadienisController';
 
 });
 
-myApp.controller('AntradienisController', function ($scope){
+myApp.controller('AntradienisController', function ($scope) {
     $scope.message = 'Hello from AntradienisController';
 
 });
-myApp.controller('TreciadienisController', function ($scope){
+myApp.controller('TreciadienisController', function ($scope) {
     $scope.message = 'Hello from TreciadienisController';
 
 });
-myApp.controller('KetvirtadienisController', function ($scope){
+myApp.controller('KetvirtadienisController', function ($scope) {
     $scope.message = 'Hello from KetvirtadienisController';
 
 });
-myApp.controller('PenktadienisController', function ($scope){
+myApp.controller('PenktadienisController', function ($scope) {
     $scope.message = 'Hello from PenktadienisController';
 
 });
 
+myApp.service('dataService', ['$http', function ($http) {
+
+    var urlBase = 'http://php.app';
+
+
+    this.getMeniu = function () {
+        return $http.get(urlBase + '/Atvaizdavimas.php' );
+    };
+
+    this.insertMeniu = function (cust) {
+        return $http.post(urlBase +'/insertas.php', cust);
+    };
+
+    // this.updateCustomer = function (cust) {
+    //     return $http.put(urlBase + '/' + cust.ID, cust)
+    // };
+
+    this.deleteMeniu = function (cust) {
+        return $http.post(urlBase + 'deletas.php', cust);
+    };
+
+}]);
